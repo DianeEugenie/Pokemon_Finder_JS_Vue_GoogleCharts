dae29cb6b1b7837a6d02bfbbb7fcad72836f78a1
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-08-30 13:20:50 +0100

    Adds components and fetches the data

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,49 @@
 <template lang="html">
+<div id="view">
+  <h1>Gotta Catch 'em All!</h1>
 
+  <div id="main-container">
+    <PokemonList :pokemons="pokemons"/>
+    <PokemonDetail />
+  </div>
+
+</div>
 </template>
 
 <script>
+import PokemonList from '@/components/PokemonList';
+import PokemonDetail from '@/components/PokemonDetail';
+
 export default {
   name: 'app',
   data() {
     return {
-      pokemons: {}
+      pokemons: [],
+      pokemon: {}
     }
   },
   mounted(){
     fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=964')
     .then(res => res.json())
     .then(data => this.pokemons = data.results)
+  },
+  components: {
+    PokemonList,
+    PokemonDetail
   }
 
 }
 </script>
 
 <style lang="css" scoped>
+#view {
+  display: flex;
+  flex-direction: column;
+}
+
+#main-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
 </style>
